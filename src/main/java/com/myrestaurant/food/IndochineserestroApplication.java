c6ce7b600402a2f0f4037fa0e4adf9f21ebdc449
neerajkr-coding <neerajkr600@gmail.com>
2024-12-22 01:18:53 +0530

    My First Commit

---
+++ src/main/java/com/myrestaurant/food/IndochineserestroApplication.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndochineserestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndochineserestroApplication.class, args);
+	}
+
+}
